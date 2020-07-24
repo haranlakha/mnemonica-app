54df2bac4d019e46746286c0f1454c2c52e772ef
haran <haran.lakha@gmail.com>
2020-07-24 20:45:29 +0100

    changed logic of random number generation so no card is displayed twice. The card displayed gets removed from the list so it cant be shown again.

--- app/src/main/java/com/example/mnemonica/TestActivity.java
+++ app/src/main/java/com/example/mnemonica/TestActivity.java
@@ -3,6 +3,7 @@ package com.example.mnemonica;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,6 +11,7 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
@@ -29,14 +31,29 @@ public class TestActivity extends AppCompatActivity {
         final TextView displayText = findViewById(R.id.displayText);
         //tamariz stack array
         String[] tamariz = {"4♣", "2♥", "7♦", "3♣", "4♥", "6♦", "A♠", "5♥", "9♠", "2♠", "Q♥", "3♦", "Q♣",
-                "8♥", "6♠", "5♠", "9♥", "K♣", "2♦", "J♥", "3♠", "8♠", "6♥", "10♣", "5♦", "K♦",
-                "2♣", "3♥", "8♦", "5♣", "K♠", "J♦", "8♣", "10♠", "K♥", "J♣", "7♠", "10♥", "A♦",
-                "4♠", "7♥", "4♦", "A♣", "9♣", "J♠", "Q♦", "7♣", "Q♠", "10♦", "6♣", "A♥", "9♦"};
-        final List<String> stack = Arrays.asList(tamariz);
+                            "8♥", "6♠", "5♠", "9♥", "K♣", "2♦", "J♥", "3♠", "8♠", "6♥", "10♣", "5♦", "K♦",
+                            "2♣", "3♥", "8♦", "5♣", "K♠", "J♦", "8♣", "10♠", "K♥", "J♣", "7♠", "10♥", "A♦",
+                            "4♠", "7♥", "4♦", "A♣", "9♣", "J♠", "Q♦", "7♣", "Q♠", "10♦", "6♣", "A♥", "9♦"};
+        final List<String> stack = new LinkedList<>(Arrays.asList(tamariz));
+
+        int numbers[] ={1,2,3,4,5,6,7,8,9,10,
+                        11,12,13,14,15,16,17,18,19,20,
+                        21,22,23,24,25,26,27,28,29,30,
+                        31,32,33,34,35,36,37,38,39,40,
+                        41,42,43,44,45,46,47,48,49,50,
+                        51,52};
+
+        final ArrayList<Integer> position =new ArrayList<Integer>();
+
+        for (int i = 0; i <= numbers.length; i++){
+            position.add(i);
+        }
 
         Random rand = new Random();
-        final int random1 = rand.nextInt(52);
+        final int random1 = rand.nextInt(stack.size());
         displayText.setText(stack.get(random1));
+        stack.remove(random1);
+        position.remove(random1);
 
         Button button = (Button)findViewById(R.id.btnEnterText);
         
@@ -46,6 +63,8 @@ public class TestActivity extends AppCompatActivity {
             public void onClick(View v) {
                 int random2 = rand2.nextInt(stack.size());
                     displayText.setText(stack.get(random2));
+                    stack.remove(random2);
+                    position.remove(random2);
 
             }
         });
